eb05763f15b328bcbdecb26c2766412c7c70bba5
yuan-fang-228 <fy0228@gmail.com>
2022-09-20 07:44:44 -0700

    followed the order in intranet

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """initialise blueprint for flask application"""
 from flask import Blueprint
-from api.v1.views.index import *
-
-
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
