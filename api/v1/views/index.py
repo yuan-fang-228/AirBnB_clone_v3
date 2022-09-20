0085c08d4330d7ffbec6d207a3cf9e22cc2bdd79
yuan-fang-228 <fy0228@gmail.com>
2022-09-20 16:18:37 -0700

    task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,23 @@
 """index page show status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+classes = {"Amenity": amenities, "City": cities,
+           "Place": places, "Review": reviews, "State": states, "User": users}
 
 
 @app_views.route("/status")
 def status():
     """show the status OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats")
+def show_stats():
+    """show the number of each objects by type"""
+    results = {}
+    for key, value in classes.items():
+        result[value] = storage.count(key)
+    return jsonify(results)
