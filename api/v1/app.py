0c62e579e7a5940934a319d03603874e35b9f719
yuan-fang-228 <fy0228@gmail.com>
2022-09-20 07:36:08 -0700

    fix pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
-
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,3 @@ m api.v1.views.index import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
