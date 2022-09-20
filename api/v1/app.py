4b4ac0b25a35ebce4fb2f65999a91bf764db9509
yuan-fang-228 <fy0228@gmail.com>
2022-09-20 14:27:05 +0000

    fix import issue

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """the first endpoint to return API status"""
 from flask import Flask, Blueprint, render_template
-from storage import models
+from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 
 
@@ -15,7 +18,7 @@ def close_currentsession(exc):
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", "5000")
-    run.app(host=host, port=port, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
 
