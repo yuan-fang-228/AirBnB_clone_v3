4808f9b08aaa21264f0f7fd3833c7f8d841e74e1
yuan-fang-228 <fy0228@gmail.com>
2022-09-20 07:40:25 -0700

    typo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """initialise blueprint for flask application"""
 from flask import Blueprint
-m api.v1.views.index import *
+from api.v1.views.index import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
