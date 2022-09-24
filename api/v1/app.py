e1eb249c47aacbf59cdfdda89cfa3db0a549ba1b
yuan-fang-228 <fy0228@gmail.com>
2022-09-24 03:34:10 -0700

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """the first endpoint to return API status"""
 from flask import Flask, Blueprint, render_template, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
